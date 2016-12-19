2469114cb72bfc2b04fd3899670647a72eb2a91b
guopengfei <1658041644@qq.com>
2016-12-19 22:07:24 +0800

    gghg

--- AXF/index.php
+++ AXF/index.php
@@ -1,13 +1,6 @@
-<?php
-require_once "jssdk.php";
-// appId  和 秘钥
-$jssdk = new JSSDK("wxaacd1f2774d0db6b", "cb8497e33dac925da032f849ca35749f");
-$signPackage = $jssdk->GetSignPackage();
-?>
 <!doctype html>
 <html lang="en">
 	<head>
-	<script type="text/javascript" src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 		<meta charset="UTF-8">
 		<title>Document</title>
 		<meta name = "viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
@@ -105,13 +98,5 @@ $signPackage = $jssdk->GetSignPackage();
 		        document.documentElement.style.fontSize=winW/10+"px";
 		    }
 		})(window);
-	wx.config({
-    debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
-    appId: '', // 必填，公众号的唯一标识
-    timestamp: , // 必填，生成签名的时间戳
-    nonceStr: '', // 必填，生成签名的随机串
-    signature: '',// 必填，签名，见附录1
-    jsApiList: [] // 必填，需要使用的JS接口列表，所有JS接口列表见附录2
-});
 	</script>
 </html>
